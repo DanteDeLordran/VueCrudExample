5e9b3907256186b8ff32141593f55185b04bafeb
Lordran <dantelopez305@outlook.com>
2023-10-21 00:35:02 -0600

    Added modal and solved render error

--- src/views/Homeworks.vue
+++ src/views/Homeworks.vue
@@ -1,46 +1,55 @@
 <script setup lang='ts' >
-import Table from '@/components/Table.vue';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import type { DataTableColumnSource } from 'vuestic-ui';
 import type { Homework } from './homeworks/interfaces/Homework';
+import axios from 'axios';
 
 const homeworks = ref<Homework[]>([])
+const perPage = 25
+const pages = computed(() => Math.ceil(homeworks.value.length / perPage))
+const page = ref(1)
+const showModal = ref(false)
+const selectedHomework = ref<Homework>({ _id: '', description: '', name: '', status: true });
+
+const columns: DataTableColumnSource<string>[] = [
+    { key: '_id', sortable: true },
+    { key: 'name', sortable: true },
+    { key: 'description', sortable: true },
+    { key: 'status', sortable: true },
+    { key: 'actions', sortable: false },
+];
 
 onMounted(async () => {
-    const data = await fetch('https://api-rest-tareas.onrender.com/api/task')
-    homeworks.value = await data.json()
+    const response = await axios.get('https://api-rest-tareas.onrender.com/api/task')
+    homeworks.value = response.data
 })
 
-const columns = [
-    {
-        key: '_id',
-        sortable: true
-    },
-    {
-        key: 'name',
-        sortable: true
-    },
-    {
-        key: 'description',
-        sortable: true
-    },
-    {
-        key: 'status',
-        sortable: true
-    },
-    {
-        key: 'actions',
-        sortable: false
-    },
-] as DataTableColumnSource<string>[];
+const editHomework = (homework: Homework) => {
+    if (homework) {
+        selectedHomework.value = homework;
+        showModal.value = true;
+    }
+};
 
-const perPage = 20
-const pages = homeworks.value.length
-const page = ref(1)
+const updateHomework = async () => {
+    if (selectedHomework.value) {
+        const index = homeworks.value.findIndex(homework => homework._id === selectedHomework.value!._id);
+        if (index !== -1) {
+            homeworks.value[index] = selectedHomework.value!;
+        }
+        showModal.value = false;
+        //await axios.put(`https://api-rest-tareas.onrender.com/api/task/`, selectedHomework.value);
+    }
+};
+
 </script>
 
 <template>
     <VaDataTable :columns="columns" :per-page="perPage" :current-page="page" :items="homeworks" striped animated>
+        <template #cell(actions)="{ rowData }">
+            <VaButton preset="plain" icon="edit" @click="editHomework(rowData)" />
+            <VaButton preset="plain" icon="delete" class="ml-3" />
+        </template>
         <template #bodyAppend>
             <tr>
                 <td colspan="6">
@@ -51,5 +60,17 @@ const page = ref(1)
             </tr>
         </template>
     </VaDataTable>
-</template>
 
+    <VaModal v-model="showModal" blur no-dismiss title="Edit Homework" hide-default-actions>
+        <VaForm class="flex flex-col items-center justify-center mb-8">
+            <VaInput v-model="selectedHomework.name" label="Name" class="w-full max-w-md" />
+            <VaInput v-model="selectedHomework.description" label="Description" class="w-full max-w-md mt-4" />
+            <VaSwitch v-model="selectedHomework.status" label="Status" class="mt-4" />
+        </VaForm>
+        <VaDivider />
+        <template #footer>
+            <VaButton preset="secondary" border-color="primary" @click="showModal = !showModal">Cancel</VaButton>
+            <VaButton class="ml-2" @click="updateHomework"> Save changes </VaButton>
+        </template>
+    </VaModal>
+</template>
